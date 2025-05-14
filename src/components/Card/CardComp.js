f0b41b5e874f0a07bc828b302160426075b53071
JoseMasiaTormo <josemasia2004@gmail.com>
2025-05-14 17:41:05 +0200

    arbol acabado

--- src/components/Card/CardComp.js
+++ src/components/Card/CardComp.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./card.css";
 
-const Card = ({ image, alt }) => {
+const Card = ({ Icon, image, label }) => {
   return (
     <div className="card">
       <div className="card2">
-        <img src={image} alt={alt} className="card-image" />
+        {image ? <img src={image} alt={label} className="card-image" /> : Icon && <Icon className="card-icon" />}
       </div>
     </div>
   );
--- src/components/CardNetwork/CardNetwork.js
+++ src/components/CardNetwork/CardNetwork.js
@@ -1,6 +1,38 @@
 import React from "react";
 import Card from "../Card/CardComp";
 import "./cardNetwork.css";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaPython,
+  FaJava,
+  FaFigma,
+  FaAws,
+  FaGitAlt,
+  FaDocker,
+} from "react-icons/fa";
+import { SiCplusplus, SiSocketdotio, SiMysql, SiMongodb } from "react-icons/si";
+import powerbiIcon from "../../assets/icons/bi.png";
+
+const iconData = [
+  { Icon: FaHtml5, label: "HTML" },
+  { Icon: FaJs, label: "JavaScript" },
+  { Icon: FaReact, label: "React" },
+  { Icon: FaCss3Alt, label: "CSS" },
+  { Icon: SiCplusplus, label: "C++" },
+  { Icon: FaPython, label: "Python" },
+  { Icon: FaJava, label: "Java" },
+  { Icon: FaFigma, label: "Figma" },
+  { image: powerbiIcon, label: "Power BI" },
+  { Icon: FaAws, label: "AWS" },
+  { Icon: FaGitAlt, label: "Git" },
+  { Icon: FaDocker, label: "Docker" },
+  { Icon: SiSocketdotio, label: "Socket.IO" },
+  { Icon: SiMysql, label: "SQL" },
+  { Icon: SiMongodb, label: "MongoDB" },
+];
 
 const cardPositions = [
   /*0HTML*/ { x: 350, y: 50 },
@@ -60,7 +92,7 @@ const CardNetwork = () => {
             top: `${pos.y}px`,
           }}
         >
-          <Card />
+          <Card Icon={iconData[index].Icon} image={iconData[index].image} label={iconData[index].label} />
         </div>
       ))}
     </div>
