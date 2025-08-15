77ee387f44936b250a58e982734678e47aaaf226
JoseMasiaTormo <josemasia2004@gmail.com>
2025-08-15 10:51:04 +0200

    background change

--- src/components/SkillsGroup/SkillsGroup.js
+++ src/components/SkillsGroup/SkillsGroup.js
@@ -8,30 +8,36 @@ import { IoLibrary } from "react-icons/io5";
 
 const SkillsGroup = () => {
   return (
-    <div className="limits">
-      <div className="skills-all">
-        <div className="skillsGroup">
-          <SkillsPart
-            title="Frontend"
-            skillsTechs={["HTML", "CSS", "JavaScript", "TypeScript", "React", "Angular"]}
-            iconSkill={<FaCode />}
-          />
-          <SkillsPart title="Backend" skillsTechs={["Python", "Java", "C++", "Node.js"]} iconSkill={<IoMdSettings />} />
-          <SkillsPart
-            title="BBDD"
-            skillsTechs={["MySQL", "MongoDB", "PostgreSQL", "DynamoDB", "PowerBI"]}
-            iconSkill={<FaDatabase />}
-          />
-          <SkillsPart
-            title="Otros"
-            skillsTechs={["Docker", "AWS", "Git", "Socket.io", "Flux", "Jest", "AWS", "Figma", "Windows", "Linux"]}
-            iconSkill={<IoLibrary />}
-          />
-        </div>
-        <div className="titleGroup">
-          <h1>
-            SKILLS & <span className="highlight-stack">STACK</span>
-          </h1>
+    <div className="background-skills">
+      <div className="limits">
+        <div className="skills-all">
+          <div className="skillsGroup">
+            <SkillsPart
+              title="Frontend"
+              skillsTechs={["HTML", "CSS", "JavaScript", "TypeScript", "React", "Angular"]}
+              iconSkill={<FaCode />}
+            />
+            <SkillsPart
+              title="Backend"
+              skillsTechs={["Python", "Java", "C++", "Node.js"]}
+              iconSkill={<IoMdSettings />}
+            />
+            <SkillsPart
+              title="BBDD"
+              skillsTechs={["MySQL", "MongoDB", "PostgreSQL", "DynamoDB", "PowerBI"]}
+              iconSkill={<FaDatabase />}
+            />
+            <SkillsPart
+              title="Otros"
+              skillsTechs={["Docker", "AWS", "Git", "Socket.io", "Flux", "Jest", "AWS", "Figma", "Windows", "Linux"]}
+              iconSkill={<IoLibrary />}
+            />
+          </div>
+          <div className="titleGroup">
+            <h1>
+              SKILLS & <span className="highlight-stack">STACK</span>
+            </h1>
+          </div>
         </div>
       </div>
     </div>
